6fa265bc1d207b7d4d1de93d41ef5cfa240c7ca5
CKNKP <kesarinandankumar2001@gmail.com>
2023-12-19 14:54:32 +0530

    Company logo added

--- Frontend_Manager/tm/src/Components/Login.js
+++ Frontend_Manager/tm/src/Components/Login.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import './Login.css';
+import React, { Component } from "react";
+import "./Login.css";
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
-      isLoggedIn: false
+      username: "",
+      password: "",
+      isLoggedIn: false,
     };
   }
 
   handleInputChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.username === 'user' && this.state.password === 'password') {
+    if (this.state.username === "user" && this.state.password === "password") {
       this.setState({ isLoggedIn: true });
     } else {
-      alert('Invalid username or password');
+      alert("Invalid username or password");
     }
-  }
+  };
 
   render() {
     if (this.state.isLoggedIn) {
@@ -31,10 +31,21 @@ class Login extends Component {
     } else {
       return (
         <form onSubmit={this.handleSubmit}>
+          <img src="https://www.ldtech.in/images/logo.png" alt="Error" className="company-logo" style={{marginBottom: "20px"}} />
           <label>Username:</label>
-          <input type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+          />
           <label>Password:</label>
-          <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
           <input type="submit" value="Login" />
         </form>
       );
