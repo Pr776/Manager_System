340fc0c6ebae287eb655021fb85d65c8dab45cfc
CKNKP <kesarinandankumar2001@gmail.com>
2023-12-19 12:59:51 +0530

    Login form

---
+++ Frontend_Manager/tm/src/Components/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './Login.css';
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      isLoggedIn: false
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.username === 'user' && this.state.password === 'password') {
+      this.setState({ isLoggedIn: true });
+    } else {
+      alert('Invalid username or password');
+    }
+  }
+
+  render() {
+    if (this.state.isLoggedIn) {
+      return <p>Welcome, {this.state.username}!</p>;
+    } else {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>Username:</label>
+          <input type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
+          <label>Password:</label>
+          <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+          <input type="submit" value="Login" />
+        </form>
+      );
+    }
+  }
+}
+
+export default Login;
