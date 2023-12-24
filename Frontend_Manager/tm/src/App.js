785aeff4fb6a65d0090767a7aa8ff5d6cffb0c3e
CKNKP <kesarinandankumar2001@gmail.com>
2023-12-24 22:17:09 +0530

    Added a login design

--- Frontend_Manager/tm/src/App.js
+++ Frontend_Manager/tm/src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './Components/Login';
-
-
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Components/Login";
+import Login2 from "./Components/Login2";
 
 function App() {
   return (
     <div className="App">
-     <Login></Login>
-     {/* <Login2></Login2> */}
+      {/* <Login></Login> */}
+      <Login2></Login2>
     </div>
   );
 }
