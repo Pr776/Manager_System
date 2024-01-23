4ce1dc8b155bd4f0686b3cb71e2efeb158cef50b
CKNKP <kesarinandankumar2001@gmail.com>
2024-01-23 16:05:03 +0530

    Check

--- Frontend_Manager/tm/src/App.js
+++ Frontend_Manager/tm/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Login2 />} />
-        {/* <Route path="/forgotpassword" element={<ForgotPassword />} /> */}
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
       </Routes>
     </div>
   );
