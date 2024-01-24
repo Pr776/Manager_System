bf5d6d989331e1210159c25691bcc86cafce4e63
CKNKP <kesarinandankumar2001@gmail.com>
2024-01-24 18:29:11 +0530

    Pie chart

--- Frontend_Manager/tm/src/Components/Dashboard.js
+++ Frontend_Manager/tm/src/Components/Dashboard.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 import React from "react";
 import {
   FaCheckCircle,
   FaTimesCircle,
   FaExclamationTriangle,
 } from "react-icons/fa";
-
 
 const DashboardContainer = styled.div`
   display: flex;
@@ -24,7 +24,7 @@ const SidebarContainer = styled.div`
   top: 0;
   left: 0;
   z-index: 1000; /* Ensure the sidebar is above other elements */
-  box-shadow: 2px 0px 5px rgba(0, 0, 0, 0.1); /* Add shadow for a bit of depth */
+  /* box-shadow: 2px 0px 5px rgba(0, 0, 0, 0.1); Add shadow for a bit of depth */
 `;
 
 const LogoContainer = styled.div`
@@ -91,18 +91,23 @@ const SignOutButton = styled.button`
 `;
 
 const StatusContainer = styled.div`
-  width: 80%;
+  width: 85%;
   display: flex;
   justify-content: center;
   align-items: center;
   position: fixed;
   top: 0;
-  left: 292px; /* Adjust the left position */
-  background-color: #fff;
+  left: 250px; /* Adjust the left position */
+  background-color: #f0f2f5;
   padding: 10px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  /* box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); */
   z-index: 1000;
   box-sizing: border-box;
+
+  @media (max-width: 1080px) {
+    width: 100%;
+    left: 0;
+  }
 `;
 
 const StatusBox = styled.div`
@@ -130,8 +135,33 @@ const StatusCount = styled.span`
   font-size: 20px;
 `;
 
+const PieChartContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  position: fixed;
+  bottom: 0;
+  left: 250px; /* Adjust the left position */
+  background-color: #f0f2f5;
+  padding: 10px;
+  z-index: 1000;
+  box-sizing: border-box;
+
+  @media (max-width: 1080px) {
+    width: 100%;
+    left: 0;
+  }
+`;
+
 const Dashboard = ({ approvedCount, rejectedCount, pendingCount }) => {
   const navigate = useNavigate();
+
+  const data = [
+    { name: "Approved", value: approvedCount },
+    { name: "Rejected", value: rejectedCount },
+    { name: "Pending", value: pendingCount },
+  ];
+
+  const COLORS = ["#4caf50", "#f44336", "#ff9800"];
 
   const handleSignOut = () => {
     // Handle sign out logic here
@@ -140,68 +170,85 @@ const Dashboard = ({ approvedCount, rejectedCount, pendingCount }) => {
   };
   return (
     <>
-    <DashboardContainer>
-      <SidebarContainer>
-        <LogoContainer>
-          {/* Add your logo here */}
-          <Logo src="https://www.ldtech.in/images/logo.png" alt="logo" />
-          {/* <CompanyName>Your Company Name</CompanyName> */}
-        </LogoContainer>
-        <MenuContainer>
-          <MenuItem>
-            <MenuLink href="/dashboard">
-              <MenuIcon className="fas fa-tachometer-alt" />
-              Dashboard
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink href="/activity-allocation">
-              <MenuIcon className="fas fa-calendar-check" />
-              Activity Allocation
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink href="/report">
-              <MenuIcon className="fas fa-file-alt" />
-              Report
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink href="/history">
-              <MenuIcon className="fas fa-history" />
-              History
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink href="/timesheet">
-              <MenuIcon className="fas fa-file-invoice" />
-              Timesheet
-            </MenuLink>
-          </MenuItem>
-        </MenuContainer>
-        <FooterContainer>
-          <SignOutButton onClick={handleSignOut}>Sign Out</SignOutButton>
-        </FooterContainer>
-      </SidebarContainer>
-      <StatusContainer>
-        <StatusBox>
-          <FaCheckCircle color="#4caf50" size={32} />
-          <StatusTitle>Approved</StatusTitle>
-          <StatusCount>{approvedCount}</StatusCount>
-        </StatusBox>
+      <DashboardContainer>
+        <SidebarContainer>
+          <LogoContainer>
+            {/* Add your logo here */}
+            <Logo src="https://www.ldtech.in/images/logo.png" alt="logo" />
+            {/* <CompanyName>Your Company Name</CompanyName> */}
+          </LogoContainer>
+          <MenuContainer>
+            <MenuItem>
+              <MenuLink href="/dashboard">
+                <MenuIcon className="fas fa-tachometer-alt" />
+                Dashboard
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink href="/activity-allocation">
+                <MenuIcon className="fas fa-calendar-check" />
+                Activity Allocation
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink href="/report">
+                <MenuIcon className="fas fa-file-alt" />
+                Report
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink href="/history">
+                <MenuIcon className="fas fa-history" />
+                History
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink href="/timesheet">
+                <MenuIcon className="fas fa-file-invoice" />
+                Timesheet
+              </MenuLink>
+            </MenuItem>
+          </MenuContainer>
+          <FooterContainer>
+            <SignOutButton onClick={handleSignOut}>Sign Out</SignOutButton>
+          </FooterContainer>
+        </SidebarContainer>
+        <StatusContainer>
+          <StatusBox>
+            <FaCheckCircle color="#4caf50" size={32} />
+            <StatusTitle>Approved</StatusTitle>
+            <StatusCount>{approvedCount}</StatusCount>
+          </StatusBox>
 
-        <StatusBox>
-          <FaTimesCircle color="#f44336" size={32} />
-          <StatusTitle>Rejected</StatusTitle>
-          <StatusCount>{rejectedCount}</StatusCount>
-        </StatusBox>
+          <StatusBox>
+            <FaTimesCircle color="#f44336" size={32} />
+            <StatusTitle>Rejected</StatusTitle>
+            <StatusCount>{rejectedCount}</StatusCount>
+          </StatusBox>
 
-        <StatusBox>
-          <FaExclamationTriangle color="#ff9800" size={32} />
-          <StatusTitle>Pending</StatusTitle>
-          <StatusCount>{pendingCount}</StatusCount>
-        </StatusBox>
-      </StatusContainer>
+          <StatusBox>
+            <FaExclamationTriangle color="#ff9800" size={32} />
+            <StatusTitle>Pending</StatusTitle>
+            <StatusCount>{pendingCount}</StatusCount>
+          </StatusBox>
+        </StatusContainer>
+        <PieChartContainer>
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+              <Pie
+                data={data}
+                innerRadius={60}
+                outerRadius={80}
+                paddingAngle={5}
+                dataKey="value"
+              >
+                {data.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index]} />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        </PieChartContainer>
       </DashboardContainer>
     </>
   );
